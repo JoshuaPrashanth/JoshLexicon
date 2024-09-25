d12a1f3317c50dadd85684f88e97da48f73c7064
Joshua Prashanth <codewithjosh12@gmail.com>
2024-09-25 20:02:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ async function dic(word) {
         const audio = new Audio();
         audio.src = data[0].phonetics[0].audio;
         audio.play();
+        audio.pause();
     })
     document.querySelector(".lin").setAttribute("href", data[0].sourceUrls[0]);
     document.querySelector(".lin").innerHTML = data[0].sourceUrls[0];
